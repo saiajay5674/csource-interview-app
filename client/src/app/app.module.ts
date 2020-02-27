9a5aa105ca55044d38befd381449adbcb5bc3122
charlescirco <charlescirco@gmail.com>
2020-02-26 19:36:28 -0500

    Working on career fairs component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CareerFairsComponent } from './career-fairs/career-fairs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CareerFairsComponent
   ],
   imports: [
     BrowserModule
