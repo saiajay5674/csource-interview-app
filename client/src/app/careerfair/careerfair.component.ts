90af8149a6534bd98c6c80c1c259881dc4987c27
charlescirco <charlescirco@gmail.com>
2020-03-02 19:50:44 -0500

    career fairs work

--- client/src/app/careerfair/careerfair.component.ts
+++ client/src/app/careerfair/careerfair.component.ts
@@ -1,30 +1,33 @@
-import { RepositoryService } from '';
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
-import { CareerFair } from 'careerfair.model';
+
+
+
+
 
 @Component({
   selector: 'app-careerfair',
-  templateUrl: 'careerfair.component.html',
-  styleUrls: ['careerfair.component.css']
+  templateUrl: './careerfair.component.html',
+  styleUrls: ['./careerfair.component.css']
 })
+
 export class CareerfairComponent implements OnInit {
 
   public displayedColumns = ['semester', 'companies', 'interviewees', 'interviews', 'details', 'update', 'delete'
 ];
-  public dataSource = new MatTableDataSource<CareerFair>();
+  public dataSource = new MatTableDataSource<CareerfairComponent>();
 
-  constructor(private repoService: RepositoryService) { }
+  constructor() { }
 
   ngOnInit() {
     this.getAllCareerFairs();
   }
 
   public getAllCareerFairs = () => {
-    this.repoService.getData('api/owner')
-    .subscribe(res => {
-      this.dataSource.data = res as CareerFair[];
-    })
+    // this.repoService.getData('api/owner')
+    // .subscribe(res => {
+    //   this.dataSource.data = res as CareerfairComponent[];
+    // })
   }
 
   public redirectToDetails = (id: string) => {
