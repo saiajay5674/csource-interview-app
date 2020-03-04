cf5f4f4fd092c9b4ac981da56f6fc5ddcf2c7bcd
charlescirco <charlescirco@gmail.com>
2020-03-03 20:31:47 -0500

    working on career fair table

---
+++ client/src/app/careerfair/career-fair.ts
@@ -0,0 +1,12 @@
+export class CareerFair {
+  semester: string;
+  companies: number;
+  interviewees: number;
+  interviews: number;
+}
+
+var arr: CareerFair[] = [
+  { "semester": "Spring 2020", "companies": 30, "interviewees": 80, "interviews": 110 },
+  { "semester": "Fall 2019", "companies": 28, "interviewees": 70, "interviews": 90 },
+  { "semester": "Spring 2019", "companies": 35, "interviewees": 90, "interviews": 100 }
+];
--- client/src/app/careerfair/careerfair.component.ts
+++ client/src/app/careerfair/careerfair.component.ts
@@ -1,45 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { stringify } from 'querystring';
 import { MatTableDataSource } from '@angular/material';
-
-
-
-
 
 @Component({
   selector: 'app-careerfair',
   templateUrl: './careerfair.component.html',
   styleUrls: ['./careerfair.component.css']
 })
-
 export class CareerfairComponent implements OnInit {
-
-  public displayedColumns = ['semester', 'companies', 'interviewees', 'interviews', 'details', 'update', 'delete'
-];
-  public dataSource = new MatTableDataSource<CareerfairComponent>();
 
   constructor() { }
 
   ngOnInit() {
-    this.getAllCareerFairs();
-  }
-
-  public getAllCareerFairs = () => {
-    // this.repoService.getData('api/owner')
-    // .subscribe(res => {
-    //   this.dataSource.data = res as CareerfairComponent[];
-    // })
-  }
-
-  public redirectToDetails = (id: string) => {
-
-  }
-
-  public redirectToUpdate = (id: string) => {
-
-  }
-
-  public redirectToDelete = (id: string) => {
-
   }
 
 }
