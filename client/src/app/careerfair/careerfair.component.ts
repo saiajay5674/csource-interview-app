3aaa217041aef9e40b89a6d7c430c7df0fd9441c
charlescirco <charlescirco@gmail.com>
2020-03-14 09:22:21 -0400

    sorting career fairs works

--- client/src/app/careerfair/careerfair.component.ts
+++ client/src/app/careerfair/careerfair.component.ts
@@ -1,25 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { stringify } from 'querystring';
 import { MatTableDataSource, MatSort } from '@angular/material';
+
+class Fairs {
+  semester: string;
+  year: number;
+  companies: number;
+  interviewees: number;
+  interviews: number;
+}
 
 @Component({
   selector: 'app-careerfair',
   templateUrl: './careerfair.component.html',
   styleUrls: ['./careerfair.component.css']
 })
-export class CareerfairComponent implements OnInit {
+export class CareerfairComponent implements OnInit, AfterViewInit {
 
   public displayedColumns = ['semester', 'year', 'companies', 'interviewees', 'interviews', 'details', 'update', 'delete'
 ];
-  public data = [{semester: "Spring", year: 2020, companies: 30, interviewees: 70, interviews: 100},
-  {semester: "Fall", year: 2019, companies: 25, interviewees: 65, interviews: 95},
-  {semester: "Spring", year: 2019, companies: 28, interviewees: 60, interviews: 90},
-  {semester: "Fall", year:2018, companies: 35, interviewees: 75, interviews: 110}];
-  public dataSource = new MatTableDataSource(this.data);
+  dataSource: MatTableDataSource<Fairs>;
+
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
 
   constructor() { }
 
   ngOnInit() {
+    const fairs: Fairs[] = [{semester: "Spring", year: 2020, companies: 30, interviewees: 70, interviews: 100},
+    {semester: "Fall", year: 2019, companies: 25, interviewees: 65, interviews: 95},
+    {semester: "Spring", year: 2019, companies: 28, interviewees: 60, interviews: 90},
+    {semester: "Fall", year:2018, companies: 35, interviewees: 75, interviews: 110}];
+    this.dataSource = new MatTableDataSource(fairs);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
 }
