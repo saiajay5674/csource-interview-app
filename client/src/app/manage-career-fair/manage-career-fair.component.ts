aa3869262a5f6c730ebdf68ec7e07b62e78a7814
leoul <leoul.emiru@gmail.com>
2020-04-08 00:52:38 -0400

    working on manage-career-fairs

--- client/src/app/manage-career-fair/manage-career-fair.component.ts
+++ client/src/app/manage-career-fair/manage-career-fair.component.ts
@@ -1,25 +1,62 @@
 import { Component, OnInit } from "@angular/core";
 import { Company } from "../_models/Company";
 import { CompanyService } from "../_services/company.service";
+import { User } from "../_models/User";
 
 @Component({
   selector: "app-manage-career-fair",
   templateUrl: "./manage-career-fair.component.html",
-  styleUrls: ["./manage-career-fair.component.css"]
+  styleUrls: ["./manage-career-fair.component.css"],
 })
 export class ManageCareerFairComponent implements OnInit {
-  company_list: Company[] = [];
+  // temp_user: User ={username}
+
+  active_company_list: Company[] = [];
+
+  inactive_company_list: Company[] = [
+    {
+      _id: "",
+      name: "google",
+      domain: "google.com",
+      companyUser: new User(),
+    },
+    {
+      _id: "",
+      name: "apple",
+      domain: "apple.com",
+      companyUser: new User(),
+    },
+    {
+      _id: "",
+      name: "ibm",
+      domain: "ibm.com",
+      companyUser: new User(),
+    },
+  ];
+
   searchText;
 
   constructor(private companyService: CompanyService) {}
 
   ngOnInit() {
-    this.loadCompanies();
+    console.log();
+    //this.loadCompanies();
   }
 
-  loadCompanies() {
-    this.companyService.getCompanies().subscribe(records => {
-      this.company_list = records;
+  deactivate($event) {
+    this.active_company_list = this.active_company_list.filter((obj) => {
+      return obj.name !== $event.name;
     });
+
+    this.inactive_company_list.push($event);
+  }
+
+  activate($event) {
+    console.log("\n\n ########activate called--\n");
+    this.inactive_company_list = this.inactive_company_list.filter((obj) => {
+      return obj.name !== $event.name;
+    });
+
+    this.active_company_list.push($event);
   }
 }
--- client/src/app/manage-company-card/manage-company-card.component.ts
+++ client/src/app/manage-company-card/manage-company-card.component.ts
@@ -1,28 +1,21 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 import { Company } from "../_models/Company";
 import { Router } from "@angular/router";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatSlideToggleChange } from "@angular/material";
 
 @Component({
   selector: "app-manage-company-card",
   templateUrl: "./manage-company-card.component.html",
-  styleUrls: ["./manage-company-card.component.css"]
+  styleUrls: ["./manage-company-card.component.css"],
 })
 export class ManageCompanyCardComponent implements OnInit {
   @Input() company: Company;
   message = "show interviwee list";
-  form: FormGroup;
 
-  constructor(private router: Router, private fb: FormBuilder) {
-    this.form = this.fb.group({
-      enable: false,
-      text: [
-        {
-          value: null,
-          disabled: true
-        }
-      ]
-    });
+  enable: boolean
+  constructor(private router: Router) {
+    //this.enable = true
   }
 
   ngOnInit() {}
@@ -31,12 +24,36 @@ export class ManageCompanyCardComponent implements OnInit {
     this.router.navigate(["/checked"]);
   }
 
-  onChange(enable: boolean) {
-    if (enable) {
-      //   console.log("\n\n !!!------");
-      //  alert("enabled");
-    } else {
-      // alert("disabled");
+  onChange($event: MatSlideToggleChange) {
+    if ($event.checked) {
+      this.activate();
+      this.enable = true;
+    } else if (!$event.checked) {
+      this.deactivate();
+      this.enable = false;
     }
+
+    console.log($event);
+  }
+
+  @Output() activateEvent = new EventEmitter<Company>();
+  activate() {
+    console.log("\n\n ########ccard is activated--\n");
+    this.activateEvent.emit({
+      _id: this.company._id,
+      name: this.company.name,
+      domain: this.company.domain,
+      companyUser: this.company.companyUser,
+    });
+  }
+
+  @Output() deactivateEvent = new EventEmitter<Company>();
+  deactivate() {
+    this.deactivateEvent.emit({
+      _id: this.company._id,
+      name: this.company.name,
+      domain: this.company.domain,
+      companyUser: this.company.companyUser,
+    });
   }
 }
