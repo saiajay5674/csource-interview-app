874e56c5afb1b84b401dcc0ca6badd2d12bb66ae
leoul <leoul.emiru@gmail.com>
2020-05-04 22:24:25 -0400

    added login validation

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,61 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../_services/auth.service'
-import { Router } from '@angular/router';
-import { first } from 'rxjs/operators';
-import { NotificationService } from '../_services/notification.service';
-import { User } from '../_models/User';
-
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../_services/auth.service";
+import { Router } from "@angular/router";
+import { first } from "rxjs/operators";
+import { NotificationService } from "../_services/notification.service";
+import { User } from "../_models/User";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
-
   username: string;
   password: string;
   currentUser: User;
+  showError: boolean = false;
 
-  constructor(private authService: AuthService,
+  constructor(
+    private authService: AuthService,
     private router: Router,
-    private notif: NotificationService) {
-      this.authService.currentUser.subscribe(x => this.currentUser = x);
+    private notif: NotificationService
+  ) {
+    this.authService.currentUser.subscribe((x) => (this.currentUser = x));
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   isAdmin() {
-    return this.currentUser && this.currentUser.role.toLowerCase() === 'admin';
+    return this.currentUser && this.currentUser.role.toLowerCase() === "admin";
   }
 
   login() {
-
     // stop here if form is invalid
     // if (this.loginForm.invalid) {
     //   return;
     // }
 
-    this.authService.login(this.username, this.password)
-      .pipe(first()).subscribe(
-        data => {
+    this.authService
+      .login(this.username, this.password)
+      .pipe(first())
+      .subscribe(
+        (data) => {
+          this.showError = false;
           console.log(data);
 
-          if(this.isAdmin()) {
-            this.router.navigate(['']);
-          }
-          else {
-            this.router.navigate(['/company', {id: data._id}]);
+          if (this.isAdmin()) {
+            this.router.navigate([""]);
+          } else {
+            this.router.navigate(["/company", { id: data._id }]);
           }
 
-          this.notif.showNotif('Logged in as: ' + this.username, 'CONFIRMATION');
+          this.notif.showNotif(
+            "Logged in as: " + this.username,
+            "CONFIRMATION"
+          );
         },
-        error => {
+        (error) => {
           // show a snackbar to user
-          this.notif.showNotif('Login Failed', 'FAILED');
-          console.log('Error', error);
-        });
+          this.showError = true;
+          this.notif.showNotif("Login Failed", "FAILED");
+          console.log("Error", error);
+        }
+      );
   }
-
 }
